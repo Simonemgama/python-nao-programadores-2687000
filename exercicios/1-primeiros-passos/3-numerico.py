633e970cc09127e6ec406ffdb12d3595092c8dbf
Simonemgama <121121515+Simonemgama@users.noreply.github.com>
2024-07-03 15:11:12 +0000

    Numerico Alterações

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,15 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+tipo_data_nascimento = type(data_nascimento)
+tipo_idade_numerica = type(idade_numerica)
+tipo_altura = type(altura)
+print(tipo_data_nascimento)
+print(tipo_idade_numerica)
+print(tipo_altura)
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+print('Simone'*2)
 
 # Realize uma operação entre dados do tipo inteiro e float
+print(10+3.5)
